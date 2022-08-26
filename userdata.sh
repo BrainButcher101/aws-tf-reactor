55106a37d1140380d81d308ddaf73a2a3f29684e
Tom <synapsenkoch27@gmail.com>
2022-08-27 00:04:26 +0200

    adjusted script to ec2

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean 2>&1
-apt-get -y install git 2>&1 
-apt-get -y install nodejs 2>&1
-apt-get -y install npm 2>&1
-cd /root
-git clone https://github.com/pranavprem/${repo_name}.git 2>&1
-cd ${repo_name}
-npm install 2>&1 
+sudo yum -y update 2>&1
+sudo yum -y git 2>&1
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash 2>&1
+. ~/.nvm/nvm.sh 2>&1
+nvm install --lts 2>&1
+git clone ${repo_name} 2>&1
+url=${repo_name}
+reponame="$(echo $url | sed -r 's/.+\/([^.]+)(\.git)?/\1/')"
+cd $reponame
+npm install 2>&1
 npm run dev 2>&1
